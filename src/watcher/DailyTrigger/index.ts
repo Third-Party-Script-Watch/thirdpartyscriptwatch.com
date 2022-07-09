2414760b1b2d7b02195eb9b56f5dce381deba2c3
Lindsay Evans <lindsay.evans@boomworks.com.au>
2022-07-09 10:13:41 +1000

    feat(watcher): switch to Puppeter for gathering metrics including sub-resource

--- src/watcher/DailyTrigger/index.ts
+++ src/watcher/DailyTrigger/index.ts
@@ -1,26 +1,9 @@
 import { AzureFunction, Context } from '@azure/functions';
 import { MongoClient } from 'mongodb';
-import * as zlib from 'zlib';
-import axios from 'axios';
 
 import * as scripts from './scripts.json';
 import { Script, ScriptMetric } from './models';
-
-const decodeBody = async function (
-  body: zlib.BrotliDecompress | zlib.Gunzip | zlib.Deflate
-): Promise<string> {
-  return new Promise((resolve, reject) => {
-    let bodyString = '';
-    body.on('data', (x) => (bodyString += x));
-    body.on('end', () => {
-      resolve(bodyString);
-    });
-    body.on('error', (e) => {
-      reject(e);
-    });
-    body.read();
-  });
-};
+import { launch } from 'puppeteer';
 
 const timerTrigger: AzureFunction = async function (
   context: Context,
@@ -33,93 +16,51 @@ const timerTrigger: AzureFunction = async function (
 
   const client = new MongoClient(connectionString);
 
-  const metrics: ScriptMetric[] = [];
-
-  const runTimestamp = new Date();
-  runTimestamp.setHours(0);
-  runTimestamp.setMinutes(0);
-  runTimestamp.setSeconds(0);
-  runTimestamp.setMilliseconds(0);
-
-  // TODO: Get DB name from env so we can run separate staging DB
-  const database = client.db('tpsw');
-  const scriptsCollection = database.collection('scripts');
-  const metricsCollection = database.collection('script-metrics');
-
-  for (let i = 0; i < scripts.length; i++) {
-    // Check if script exists in DB, add if not
-    let script = await scriptsCollection.findOne<Script>({
-      id: scripts[i].id,
-    });
-
-    if (script === null) {
-      const result = await scriptsCollection.insertOne({
-        id: scripts[i].id,
-        name: scripts[i].name,
-        url: scripts[i].url,
-      });
-
-      if (!result.acknowledged) {
-        throw new Error(`Error inserting new script ${scripts[i].id}`);
-      }
-      script = await scriptsCollection.findOne<Script>({ id: scripts[i].id });
-    }
-
-    const response = await axios.get(scripts[i].url, {
-      // Workaround to support Brotli:
-      // https://github.com/axios/axios/issues/1635#issuecomment-603258750
-      headers: {
-        'accept-encoding': 'br, gzip, deflate',
-      },
-      decompress: false,
-      responseType: 'stream',
-      // if you want to enhance the default transformResponse, instead of replacing,
-      // use an array to contain both the default and the customized
-      transformResponse(data) {
-        if (data.headers['content-encoding'] === 'br') {
-          return data.pipe(zlib.createBrotliDecompress());
-        }
-        if (data.headers['content-encoding'] === 'gzip') {
-          return data.pipe(zlib.createGunzip());
-        }
-        if (data.headers['content-encoding'] === 'deflate') {
-          return data.pipe(zlib.createDeflate());
-        }
-
-        return data;
-      },
-    });
-
-    const headers = response.headers;
-    const content = await decodeBody(response.data);
-
-    metrics.push({
-      script: script._id,
-      retrieved: runTimestamp,
-      contentType: headers['content-type'],
-      contentEncoding: headers['content-encoding'],
-      contentLength: parseInt(headers['content-length'], 10),
-      contentLengthUncompressed: content.length,
-    });
-  }
-
-  const result = await metricsCollection.insertMany(metrics);
-  if (result.acknowledged) {
-    console.log(`Inserted ${result.insertedCount} metrics`);
-  } else {
-    throw new Error('Error inserting metrics');
-  }
-
-  // Grab metrics for last 30 days & output JSON file to blob storage
-  const metricsData = [];
+  const metrics = await getMetrics();
 
   try {
-    // TODO: Get DB name from env so we can run separate staging DB
-    const database = client.db('tpsw');
+    const database = client.db(process.env.MONGODB_DATABASE || 'tpsw');
     const scriptsCollection = database.collection('scripts');
     const metricsCollection = database.collection('script-metrics');
 
-    const scripts = await scriptsCollection.find<Script>({}).toArray();
+    for (let i = 0; i < scripts.length; i++) {
+      // Check if script exists in DB, add if not
+      let script = await scriptsCollection.findOne<Script>({
+        id: scripts[i].id,
+      });
+      if (script === null) {
+        const result = await scriptsCollection.insertOne({
+          id: scripts[i].id,
+          name: scripts[i].name,
+          url: scripts[i].url,
+        });
+
+        if (!result.acknowledged) {
+          throw new Error(`Error inserting new script ${scripts[i].id}`);
+        }
+        script = await scriptsCollection.findOne<Script>({ id: scripts[i].id });
+      }
+
+      // Link metrics to script
+      metrics
+        .filter((x) => x.scriptId === script.id)
+        .forEach((x) => {
+          x.script = script._id;
+          x.scriptId = undefined;
+        });
+    }
+
+    const result = await metricsCollection.insertMany(metrics);
+    if (result.acknowledged) {
+      console.log(`Inserted ${result.insertedCount} metrics`);
+    } else {
+      throw new Error('Error inserting metrics');
+    }
+
+    // Grab metrics for last 30 days & output JSON file to blob storage
+    const metricsLast30Data = [];
+
+    const existingScripts = await scriptsCollection.find<Script>({}).toArray();
 
     const scriptMetricsLimit = new Date();
     scriptMetricsLimit.setDate(scriptMetricsLimit.getDate() - 30);
@@ -129,7 +70,7 @@ const timerTrigger: AzureFunction = async function (
       })
       .toArray();
 
-    scripts.forEach((script) => {
+    existingScripts.forEach((script) => {
       script.metrics = scriptMetricsPast30Days
         .filter((x) => x.script.equals(script._id))
         .map((x) => ({
@@ -147,15 +88,73 @@ const timerTrigger: AzureFunction = async function (
           return -1;
         });
 
-      metricsData.push({ ...script, _id: undefined });
+      metricsLast30Data.push({ ...script, _id: undefined });
     });
+
+    context.bindings.dataOutput = JSON.stringify(metricsLast30Data);
   } catch (e) {
     console.error(e);
   } finally {
     await client.close();
   }
-
-  context.bindings.dataOutput = JSON.stringify(metricsData);
 };
+
+async function getMetrics(): Promise<ScriptMetric[]> {
+  const metrics = [];
+
+  const browser = await launch();
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
+  await page.setCacheEnabled(false);
+
+  const retrieved = new Date();
+  retrieved.setHours(0);
+  retrieved.setMinutes(0);
+  retrieved.setSeconds(0);
+  retrieved.setMilliseconds(0);
+
+  for (let i = 0; i < scripts.length; i++) {
+    const script = scripts[i];
+
+    page.on('requestfinished', async (req) => {
+      const response = req.response();
+      if (response !== null) {
+        const headers = response.headers();
+        const content = await response.buffer();
+
+        metrics.push({
+          scriptId: script.id,
+          retrieved,
+          url: response.url(),
+          isInitialRequest: response.url() === script.url,
+          contentType: headers['content-type'],
+          contentEncoding: headers['content-encoding'],
+          contentLength: parseInt(headers['content-length'], 10),
+          contentLengthUncompressed: content.byteLength,
+        });
+      }
+    });
+
+    await page.setContent(
+      `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8" />
+            <title>${script.name}</title>
+        </head>
+        <body>
+            <h1>${script.name}</h1>
+            <p>ID: ${script.id}</p>
+            ${script.initialisationHtml}
+        </body>
+        </html>`,
+      { waitUntil: 'networkidle0' }
+    );
+  }
+
+  await browser.close();
+
+  return metrics;
+}
 
 export default timerTrigger;
--- src/watcher/DailyTrigger/models/script-metric.ts
+++ src/watcher/DailyTrigger/models/script-metric.ts
@@ -2,7 +2,10 @@ import { ObjectId } from 'mongodb';
 
 export type ScriptMetric = {
   script?: ObjectId;
+  scriptId?: string;
   retrieved: Date;
+  url: string;
+  isInitialRequest: boolean;
   contentType: string;
   contentEncoding: string;
   contentLength: number;

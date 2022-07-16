2231206d4c1f883145bad3a4cfd93789c158d5e8
Lindsay Evans <lindsay.evans@boomworks.com.au>
2022-07-16 10:25:58 +1000

    feat(templating): implement next update countdown in Vue

--- src/ui/components/Filter.vue
+++ src/ui/components/Filter.vue
@@ -1,7 +1,7 @@
 <template>
     <form>
         <h2 class="results-heading">Last 30 days results</h2>
-        <p class="next-update"></p>
+        <p class="next-update" :title="nextUpdateTitle">{{ nextUpdateText }}</p>
         <div class="filter">
             <input name="q" id="q" type="search" placeholder=" " v-model="keywords" @input="onKeywordsChange" />
             <label for="q">Filter by name or URL</label>
@@ -14,7 +14,12 @@ import { onMounted, ref } from 'vue';
 
 const emit = defineEmits(['filterChange'])
 
-const keywords = ref()
+const keywords = ref();
+
+const nextUpdateText = ref();
+const nextUpdateTitle = ref();
+
+let nextUpdateTimer;
 
 onMounted(() => {
     const params = new URLSearchParams(document.location.search);
@@ -25,6 +30,12 @@ onMounted(() => {
             emit('filterChange', keywordsParam);
         }
     }
+
+
+    nextUpdateTimer = setInterval(() => {
+        setNextUpdate();
+    }, 60000);
+    setNextUpdate();
 })
 
 function onKeywordsChange() {
@@ -35,4 +46,47 @@ function onKeywordsChange() {
         keywords.value === '' ? window.location.pathname : `?q=${keywords.value}`
     );
 }
+
+
+function setNextUpdate() {
+    const now = new Date();
+    let nextUpdate = new Date();
+    nextUpdate.setDate(nextUpdate.getDate() + 1);
+    nextUpdate = new Date(
+        Date.UTC(
+            nextUpdate.getUTCFullYear(),
+            nextUpdate.getUTCMonth(),
+            nextUpdate.getUTCDate()
+        )
+    );
+
+    nextUpdateText.value = getRemaining(now, nextUpdate);
+    nextUpdateTitle.value = `${nextUpdate.toLocaleString()} local time (midnight UTC)`
+}
+
+function getRemaining(now: Date, then: Date): string {
+    const seconds = Math.round((then.getTime() - now.getTime()) / 1000);
+    const minutes = Math.round(seconds / 60);
+    let remaining = 'Data will update in ';
+
+    if (minutes > 60) {
+        remaining += `${Math.round(minutes / 60 - 1)}h `;
+    }
+    if (seconds > 60) {
+        remaining += `${Math.round(minutes % 60)}m`;
+    }
+    if (seconds <= 60) {
+        remaining += `${seconds}s`;
+        nextUpdateTimer = setInterval(() => {
+            setNextUpdate();
+        }, 1000);
+    }
+    if (seconds <= 0) {
+        remaining = 'Data updated - reload to view';
+        clearInterval(nextUpdateTimer);
+    }
+
+    return remaining;
+}
+
 </script>
